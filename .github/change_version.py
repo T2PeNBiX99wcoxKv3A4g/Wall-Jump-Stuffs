810abd1d527b0e0e438f5eaefd6e134593fba088
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-04-05 20:49:24 +0800

    fix(Workflows): Change propertie name and add `requirements.txt` file

--- .github/change_version.py
+++ .github/change_version.py
@@ -2,9 +2,9 @@
 
 import argparse
 
-if __name__ == "__main__":
-    # from jproperties import Properties
+propertieName = 'version'
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('version', help="New version", type=str)
     args = parser.parse_args()
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
         # Bad
         for line in file.readlines():
-            start = line.find('mod_version')
+            start = line.find(propertieName)
 
             if start < 0:
                 old_text += line
@@ -27,7 +27,6 @@ if __name__ == "__main__":
 
             old_version = line[start + 12:-1]
             print(f'Old version: {old_version}, New version: {new_version}')
-            # file.writelines(f'mod_version={new_version}\n')
             old_text += line
 
         new_text = old_text.replace(old_version, new_version)
@@ -36,17 +35,3 @@ if __name__ == "__main__":
         file.truncate(0)
         file.write(new_text)
         file.close()
-
-        # configs = Properties()
-        # configs.load(file)
-        # 
-        # new_version: str = args.version
-        # new_version.replace('"', '')
-        # 
-        # configs['mod_version'] = new_version[1:]
-        # 
-        # print(f'New Version: {str(configs['mod_version'])}')
-        # 
-        # file.seek(0)
-        # file.truncate(0)
-        # configs.store(file)
